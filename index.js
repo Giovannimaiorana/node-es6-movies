6f191a383b411877ce59672d5ac03710ed36a739
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-11-08 14:42:56 +0100

    creation class movie and class tvseries with const

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*Definire un array di oggetti; ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv) */
-const mediaCatalog = [
+const movie = [
     {
         title: "The Shawshank Redemption",
         year: 1994,
@@ -14,7 +14,7 @@ const mediaCatalog = [
         genre: "Crime, Drama, Thriller",
         rating: 9.5,
         type: "tv",
-        season: 5,
+        seasons: 5,
         price: 3.99,
     },
     {
@@ -31,7 +31,43 @@ const mediaCatalog = [
         genre: "Drama, Fantasy, Horror",
         rating: 8.7,
         type: "tv",
-        season: 4,
+        seasons: 4,
         price: 3.99,
     },
 ];
+
+/*Creare una classe Movie che contenga le informazioni sopra indicate. */
+
+class Movie {
+    title;
+    year;
+    genre;
+    rating;
+    type;
+    price;
+
+    /*constructor*/
+    constructor(title, year, genre, rating, type, price) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+        this.type = type;
+        this.price = price;
+    }
+
+
+
+}
+/* Creare una classe TvSeries che estenda la classe Movie e ne aggiunta la proprietà seasons.*/
+
+class TvSeries extends Movie {
+    seasons;
+
+    /*Constructor con super per prendere dati da padre  */
+    constructor(title, year, genre, rating, type, price, seasons) {
+        super(title, year, genre, rating, type, price);
+        this.seasons = seasons;
+    }
+}
+
