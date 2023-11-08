25847cbf4a66ea9d0029ece41d696af955721508
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-11-08 14:20:28 +0100

    start project -Creation Array

---
+++ index.js
@@ -0,0 +1,37 @@
+/*Definire un array di oggetti; ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv) */
+const mediaCatalog = [
+    {
+        title: "The Shawshank Redemption",
+        year: 1994,
+        genre: "Drama",
+        rating: 9.3,
+        type: "movie",
+        price: 3.99,
+    },
+    {
+        title: "Breaking Bad",
+        year: 2008,
+        genre: "Crime, Drama, Thriller",
+        rating: 9.5,
+        type: "tv",
+        season: 5,
+        price: 3.99,
+    },
+    {
+        title: "Inception",
+        year: 2010,
+        genre: "Action, Adventure, Sci-Fi",
+        rating: 8.8,
+        type: "movie",
+        price: 3.99,
+    },
+    {
+        title: "Stranger Things",
+        year: 2016,
+        genre: "Drama, Fantasy, Horror",
+        rating: 8.7,
+        type: "tv",
+        season: 4,
+        price: 3.99,
+    },
+];
